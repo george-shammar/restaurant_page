aef438a2afc672a9643a9e5ea59d08696709dbdb
george-shammar <george.gbenle@outlook.com>
2021-04-29 21:59:42 +0000

    Create contact tab

--- src/page-load.js
+++ src/page-load.js
@@ -55,6 +55,18 @@ function menu() {
     linkMenu.appendChild(homeMenuText);
     homeMenu.appendChild(linkMenu);
     nav.appendChild(homeMenu);
+
+
+    // Create contact tab
+    const homeContact = document.createElement('div');
+    homeContact.setAttribute('class', 'primary-button me-5');
+    const linkContact = document.createElement('a');
+    linkContact.setAttribute('href', "contact.html");
+    const homeContactText = document.createTextNode('Contact');
+
+    linkContact.appendChild(homeContactText);
+    homeContact.appendChild(linkContact);
+    nav.appendChild(homeContact);
 }
 
 export {pageLayout}
