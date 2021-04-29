1bc24467ce42334553bbc667cdbd2ef484ee2fce
george-shammar <george.gbenle@outlook.com>
2021-04-29 20:10:25 +0000

    create container div

--- src/page-load.js
+++ src/page-load.js
@@ -2,10 +2,13 @@ function pageLoad() {
     const banner = document.createElement('div');
     banner.setAttribute('class', 'banner');
 
+    const container = document.createElement('div');
+    banner.setAttribute('class', 'container');
+
     const content = document.getElementById('content')
     content.appendChild(banner);
 
-    
+
 }
 
 export default pageLoad
