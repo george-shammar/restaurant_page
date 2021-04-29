b2923e4324e0e75535724dec3c5589f54e987768
george-shammar <george.gbenle@outlook.com>
2021-04-29 21:37:50 +0000

    Refactor import for menu function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // import pageLoad function from page-load
-// import pageLayout from './page-load';
-import menu from './page-load';
 
 import { pageLayout } from './page-load';
+import { menu} from './page-load';
 
 pageLayout();
 menu();
