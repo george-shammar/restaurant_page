860092930d59084dba6de75f349a1cc08bea742b
george-shammar <george.gbenle@outlook.com>
2021-04-29 21:20:59 +0000

    Export and import the menu function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // import pageLoad function from page-load
-import pageLayout from "./page-load";
+import pageLayout from './page-load';
+import menu from './page-load';
 
 pageLayout();
+menu();
--- src/page-load.js
+++ src/page-load.js
@@ -33,3 +33,4 @@ function menu() {
 }
 
 export default pageLayout
+export default menu
