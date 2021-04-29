f13c4a40eda53763891aef409a5986317c795595
george-shammar <george.gbenle@outlook.com>
2021-04-29 21:24:34 +0000

    Refactor code

--- src/page-load.js
+++ src/page-load.js
@@ -1,10 +1,11 @@
 import './style.css';
 
+const content = document.getElementById('content');
+
 function pageLayout() {
     // create banner div and append to content.
     const banner = document.createElement('div');
     banner.setAttribute('class', 'banner');
-    const content = document.getElementById('content')
     content.appendChild(banner);
 
     // create container div and append to banner
@@ -29,6 +30,8 @@ function pageLayout() {
 
 
 function menu() {
+    const nav = document.createElement('nav');
+    nav.setAttribute('class', 'menu');
 
 }
 
