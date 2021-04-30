2a89d47fc62b2b4dac7ed4a10622fe06b5f34508
george-shammar <george.gbenle@outlook.com>
2021-04-30 16:11:21 +0000

    Update menu page

---
+++ src/menus.js
@@ -0,0 +1,35 @@
+import './style.css';
+
+
+const menu = document.getElementById('menu');
+
+function contactPage() {
+    // create heading
+    const heading = document.createElement('div');
+    heading.setAttribute('class', 'page-heading');
+    contact.appendChild(heading);
+
+    // Create container
+    const containerContact = document.createElement('div');
+    containerContact.setAttribute('class', 'container');
+    heading.appendChild(containerContact);
+
+     // Create header with text
+     const headerContact = document.createElement('h1');
+     headerContact.setAttribute('class', 'text');
+     const headerContactText =  document.createTextNode('Contact Us');
+     headerContact.appendChild(headerContactText);
+     containerContact.appendChild(headerContact);
+
+    // Create header paragrapgh
+    const paragraphContact = document.createElement('p');
+    paragraphContact.setAttribute('class', 'text');
+    const paragraphContactText = document.createTextNode(' East Legon; P.O. Box KD213 Kanda,. Accra, GHANA Tel: 233');
+    paragraphContact.appendChild(paragraphContactText);
+    containerContact.appendChild(paragraphContact);
+
+}
+
+contactPage();
+
+export {contactPage}
