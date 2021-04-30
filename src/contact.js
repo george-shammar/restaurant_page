06752a56ffc1e31e6542e676c603627e2056508b
george-shammar <george.gbenle@outlook.com>
2021-04-30 11:04:00 +0000

    Set conatiner

--- src/contact.js
+++ src/contact.js
@@ -3,12 +3,19 @@ import './style.css';
 
 const contact = document.getElementById('contact');
 
-function contact() {
+function contactPage() {
+    // create heading
     const heading = document.createElement('div');
     heading.setAttribute('class', 'page-heading');
-    
+    contact.appendChild(heading);
+
+    // Create container
+    const containerContact = document.createElement('div');
+    containerContact.setAttribute('class', 'container');
+    heading.appendChild(container);
+
 
 }
 
 
-export {contact}
+export {contactPage}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import funtcions from page-load.js
 import { pageLayout } from './page-load';
 import { menu} from './page-load';
-import {contact} from './contact';
+import {contactPage} from './contact';
 
 pageLayout();
 menu();
