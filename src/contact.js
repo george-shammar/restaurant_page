8186627042c03f1d3081402edbb511e0e44c1452
george-shammar <george.gbenle@outlook.com>
2021-05-01 16:06:19 +0000

    fix linter errors

--- src/contact.js
+++ src/contact.js
@@ -1,42 +1,42 @@
 import './style.css';
-import {menu} from './page-load';
+import { menu } from './page-load';
 
 function contactPage() {
-    // create heading
-    const contact = document.createElement('div');
-    contact.setAttribute('class', 'contact');
-    const heading = document.createElement('div');
-    heading.setAttribute('class', 'page-heading');
-    contact.appendChild(heading);
+  // create heading
+  const contact = document.createElement('div');
+  contact.setAttribute('class', 'contact');
+  const heading = document.createElement('div');
+  heading.setAttribute('class', 'page-heading');
+  contact.appendChild(heading);
 
-    // Create container
-    const containerContact = document.createElement('div');
-    containerContact.setAttribute('class', 'container');
-    heading.appendChild(containerContact);
+  // Create container
+  const containerContact = document.createElement('div');
+  containerContact.setAttribute('class', 'container');
+  heading.appendChild(containerContact);
 
-     // Create header with text
-     const headerContact = document.createElement('h1');
-     headerContact.setAttribute('class', 'text');
-     const headerContactText =  document.createTextNode('Contact Us');
-     headerContact.appendChild(headerContactText);
-     containerContact.appendChild(headerContact);
+  // Create header with text
+  const headerContact = document.createElement('h1');
+  headerContact.setAttribute('class', 'text');
+  const headerContactText =  document.createTextNode('Contact Us');
+  headerContact.appendChild(headerContactText);
+  containerContact.appendChild(headerContact);
 
-    // Create header paragrapgh
-    const paragraphContact = document.createElement('p');
-    paragraphContact.setAttribute('class', 'text');
-    const paragraphContactText = document.createTextNode(' East Legon; P.O. Box KD213 Kanda,. Accra, GHANA Tel: 233');
-    paragraphContact.appendChild(paragraphContactText);
-    containerContact.appendChild(paragraphContact);
+  // Create header paragrapgh
+  const paragraphContact = document.createElement('p');
+  paragraphContact.setAttribute('class', 'text');
+  const paragraphContactText = document.createTextNode(' East Legon; P.O. Box KD213 Kanda,. Accra, GHANA Tel: 233');
+  paragraphContact.appendChild(paragraphContactText);
+  containerContact.appendChild(paragraphContact);
 
-    menu();
+  menu();
 
-    return contact;
+  return contact;
 }
 
 function callContact() {
-    const pageSwitch = document.getElementById("content");
-    pageSwitch.innerHTML = '';
-    pageSwitch.appendChild(contactPage());
+  const pageSwitch = document.getElementById("content");
+  pageSwitch.innerHTML = '';
+  pageSwitch.appendChild(contactPage());
 }
   
 
