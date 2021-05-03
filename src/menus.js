7da62771b94c4a69631c031328b5acbbbb59293a
george-shammar <george.gbenle@outlook.com>
2021-05-03 12:00:37 +0000

    Add node_modules and .vscode to .gitignore

---
+++ src/menus.js
@@ -0,0 +1,39 @@
+import './style.css';
+
+function menuPage() {
+  // create heading
+  const menuMenu = document.createElement('div');
+  menuMenu.setAttribute('class', 'menu');
+  const headingMenu = document.createElement('div');
+  headingMenu.setAttribute('class', 'page-heading');
+  menuMenu.appendChild(headingMenu);
+
+  // Create container
+  const containerMenu = document.createElement('div');
+  containerMenu.setAttribute('class', 'container');
+  headingMenu.appendChild(containerMenu);
+
+  // Create header with text
+  const headerMenu = document.createElement('h1');
+  headerMenu.setAttribute('class', 'text');
+  const headerMenuText = document.createTextNode('Our Menu');
+  headerMenu.appendChild(headerMenuText);
+  containerMenu.appendChild(headerMenu);
+
+  // Create header paragrapgh
+  const paragraphMenu = document.createElement('p');
+  paragraphMenu.setAttribute('class', 'text');
+  const paragraphMenuText = document.createTextNode('Breakfast......Brunch......Lunch......Dinner');
+  paragraphMenu.appendChild(paragraphMenuText);
+  containerMenu.appendChild(paragraphMenu);
+
+  return menuMenu;
+}
+
+function callMenu() {
+  const pageSwitchMenu = document.getElementById('content');
+  pageSwitchMenu.innerHTML = '';
+  pageSwitchMenu.appendChild(menuPage());
+}
+
+export default callMenu;
---
+++ src/page-load.js
@@ -0,0 +1,72 @@
+import './style.css';
+
+const content = document.getElementById('content');
+
+function pageLayout() {
+  // create banner div and append to content.
+  const banner = document.createElement('div');
+  banner.setAttribute('class', 'banner');
+  content.appendChild(banner);
+
+  // create container div and append to banner
+  const container = document.createElement('div');
+  container.setAttribute('class', 'container');
+  banner.appendChild(container);
+
+  // Create header with text
+  const header = document.createElement('h1');
+  header.setAttribute('class', 'text');
+  const headerText = document.createTextNode('Bon Goût');
+  header.appendChild(headerText);
+  container.appendChild(header);
+
+  // Create header paragrapgh
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'text');
+  const paragraphText = document.createTextNode('We tantalize your taste bud...');
+  paragraph.appendChild(paragraphText);
+  container.appendChild(paragraph);
+
+  // create menu
+  const nav = document.createElement('nav');
+  nav.setAttribute('class', 'menu');
+  content.appendChild(nav);
+
+  // create home tab
+  const home = document.createElement('div');
+  home.setAttribute('class', 'primary-button me-5');
+  const link = document.createElement('a');
+  link.setAttribute('href', '#');
+  link.setAttribute('id', 'home');
+  const homeText = document.createTextNode('Home');
+
+  link.appendChild(homeText);
+  home.appendChild(link);
+  nav.appendChild(home);
+
+  // Create menu tab
+  const homeMenu = document.createElement('div');
+  homeMenu.setAttribute('class', 'primary-button me-5');
+  const linkMenu = document.createElement('a');
+  linkMenu.setAttribute('href', '#');
+  linkMenu.setAttribute('id', 'menu');
+  const homeMenuText = document.createTextNode('Menu');
+
+  linkMenu.appendChild(homeMenuText);
+  homeMenu.appendChild(linkMenu);
+  nav.appendChild(homeMenu);
+
+  // Create contact tab
+  const homeContact = document.createElement('div');
+  homeContact.setAttribute('class', 'primary-button me-5');
+  const linkContact = document.createElement('a');
+  linkContact.setAttribute('href', '#');
+  linkContact.setAttribute('id', 'contact');
+  const homeContactText = document.createTextNode('Contact');
+
+  linkContact.appendChild(homeContactText);
+  homeContact.appendChild(linkContact);
+  nav.appendChild(homeContact);
+}
+
+export default pageLayout;
